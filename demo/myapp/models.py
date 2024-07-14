11f5130d2249b232da70be53d05d284de464f26f
Jinn Lim <jinnlk@berkeley.edu>
2024-07-13 17:36:11 -0700

    worked on rest api framework

--- demo/myapp/models.py
+++ demo/myapp/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class TodoItem(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
